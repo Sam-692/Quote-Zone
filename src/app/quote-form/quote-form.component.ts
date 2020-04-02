68467b035c9bda7c32c800768c0a55efe2c71699
Sam-692 <samuelkariuki786@gmail.com>
2020-04-03 00:53:20 +0300

    form submit

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,23 +1,23 @@
-// import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-// import { Quote } from '../quote';
+import { Quote } from '../quote';
 
-// @Component({
-//   selector: 'app-quote-form',
-//   templateUrl: './quote-form.component.html',
-//   styleUrls: ['./quote-form.component.css']
-// })
-// export class QuoteFormComponent implements OnInit {
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
 
-//   newQuote = new Quote(0,"","",new Date());
-//   @Output() addQuote = new EventEmitter<Quote>();
-//   submitQuote(){
-//     this.addQuote.emit(this.newQuote);
-//       }
+  newQuote = new Quote(0,"","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+      }
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
