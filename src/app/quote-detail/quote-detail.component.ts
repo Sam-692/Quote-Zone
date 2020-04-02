ea68e29169e7d67b6c2cc3851e61290b179b8da2
Sam-692 <samuelkariuki786@gmail.com>
2020-04-03 00:49:39 +0300

    detail deliting quote

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,38 +1,38 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 
-// @Component({
-//   selector: 'app-quote-detail',
-//   templateUrl: './quote-detail.component.html',
-//   styleUrls: ['./quote-detail.component.css']
-// })
-// export class QuoteDetailComponent implements OnInit {
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
 
-//   public up = 0;
-//   public down = 0;
+  public up = 0;
+  public down = 0;
 
-// @Input() quote: Quote;
-// @Output() isComplete = new EventEmitter<boolean>();
+@Input() quote: Quote;
+@Output() isComplete = new EventEmitter<boolean>();
 
-// quoteComplete(complete:boolean){
-//   this.isComplete.emit(complete);
-// }
+quoteComplete(complete:boolean){
+  this.isComplete.emit(complete);
+}
 
-// onClick(){
+onClick(){
   
-//   this.up++;
-// }
-// voteDown(){
-//   this.down++;
-// }
+  this.up++;
+}
+voteDown(){
+  this.down++;
+}
 
-// quoteDelete(complete:boolean){
-//   this.isComplete.emit(complete)
-// }
-//   constructor() { }
+quoteDelete(complete:boolean){
+  this.isComplete.emit(complete)
+}
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
