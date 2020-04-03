4890a1e2521cd5ab644a6d28a6a672d0056c1894
Sam-692 <samuelkariuki786@gmail.com>
2020-04-03 08:49:47 +0300

    styling qoutes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -15,6 +15,7 @@ export class QuoteFormComponent implements OnInit {
     this.addQuote.emit(this.newQuote);
       }
 
+
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,8 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=  [
     new Quote(1, 'In the end, its not the years in your life that count. Its the life in your years. ', 'Abraham Lincoln',new Date(2020,3,14)),
-    // new Quote(2,'Life is inherently risky. There is only one big risk you should avoid at all costs, and that is the risk of doing nothing.','George Bernard Shaw',new Date(2016,6,9)),
-    new Quote(3,'Life isnt about finding yourself. Life is about creating yourself.','Denis Waitley',new Date(2010,4,12)),
-    new Quote(4,'I have found that if you love life, life will love you back.','Arthur Rubinstein',new Date(2012,7,29)),
-    new Quote(5,'We all have two lives. The second one starts when we realize we only have one.','Mtoto wa nursary',new Date(2000,2,29)),
+    new Quote(2,'Life isnt about finding yourself. Life is about creating yourself.','Denis Waitley',new Date(2010,4,12)),
+    new Quote(3,'I have found that if you love life, life will love you back.','Arthur Rubinstein',new Date(2012,7,29)),
   ];
 
   toggleDetails(index){
